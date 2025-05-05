81a00d5b9566d39c6043a878908ff503f4dd1b07
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-05-04 22:46:04 -0300

    feat(blog): add a last warning about blog desactivation

--- sw.js
+++ sw.js
@@ -1,36 +1,13 @@
-const CACHE_NAME = "kitsune-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/main.css",
-  "/manifest.json",
-  "/favicon.ico",
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open("foxtech-cache")
+      .then((cache) => cache.addAll(["/", "/index.html"])),
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        }),
-      ),
-    ),
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
